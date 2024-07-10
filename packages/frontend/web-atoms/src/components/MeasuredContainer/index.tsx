c641ed3c632d3d05a28c5169edd0cbb2b793cd79
Vuong Ngo <vuongngo.pd@gmail.com>
2024-07-10 21:58:08 +1000

    feat(frontend-web-atoms): measuredComponent
    Fix ui issue with MeasureComponent

--- packages/frontend/web-atoms/src/components/MeasuredContainer/index.tsx
+++ packages/frontend/web-atoms/src/components/MeasuredContainer/index.tsx
@@ -5,14 +5,16 @@ interface IMeasuredContainerProps extends React.ComponentPropsWithoutRef<'div'> 
 
 export const MeasuredContainer = ({ children, className }: IMeasuredContainerProps) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [rect, setRect] = useState<{ width?: number; height?: number }>({});
+  const [rect, setRect] = useState<{ width?: number }>({});
   useEffect(() => {
-    if (ref.current) {
+    if (!ref.current) return;
+    const resizeObserver = new ResizeObserver(() => {
       setRect({
-        width: ref.current.clientWidth,
-        height: ref.current.clientHeight,
+        width: ref.current?.clientWidth,
       });
-    }
+    });
+    resizeObserver.observe(ref.current);
+    return () => resizeObserver.disconnect();
   }, []);
   return (
     <div className={className} ref={ref} style={rect}>
